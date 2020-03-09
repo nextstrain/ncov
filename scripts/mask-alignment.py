b6f36d809654f32a800167f033fb6f020846b061
terrycojones <terry@jon.es>
2020-03-10 00:05:42 +0100

    Minor changes to mask-alignment.py

--- scripts/mask-alignment.py
+++ scripts/mask-alignment.py
@@ -12,29 +12,29 @@ if __name__ == '__main__':
         description="Mask initial bases from alignment FASTA",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    parser.add_argument("--alignment", required=True, help="FASTA file of alignment")
-    parser.add_argument("--mask-from-beginning", type = int, required=True, help="number of bases to mask from start")
-    parser.add_argument("--mask-from-end", type = int, help="number of bases to mask from end")
-    parser.add_argument("--mask-sites", nargs='+', type = int,  help="list of sites to mask")
-    parser.add_argument("--output", required=True, help="FASTA file of output alignment")
+    parser.add_argument("--alignment", required=True,
+                        help="FASTA file of alignment")
+    parser.add_argument("--mask-from-beginning", type=int, required=True,
+                        dest='begin_length', default=0,
+                        help="number of bases to mask from start")
+    parser.add_argument("--mask-from-end", type=int,
+                        dest='end_length', default=0,
+                        help="number of bases to mask from end")
+    parser.add_argument("--mask-sites", nargs='+', type=int, default=[],
+                        help="list of (1-based) sites to mask")
+    parser.add_argument("--output", required=True,
+                        help="FASTA file of output alignment")
     args = parser.parse_args()
 
-    being_length = 0
-    if args.mask_from_beginning:
-        begin_length = args.mask_from_beginning
-    end_length = 0
-    if args.mask_from_end:
-        end_length = args.mask_from_end
+    start = ["N"] * args.begin_length
+    end = ["N"] * args.end_length
 
     with open(args.output, 'w') as outfile:
         for record in Bio.SeqIO.parse(args.alignment, 'fasta'):
             seq = str(record.seq)
-            start = "N" * begin_length
-            middle = seq[begin_length:-end_length]
-            end = "N" * end_length
-            seq_list = list(start + middle + end)
-            if args.mask_sites:
-                for site in args.mask_sites:
-                    seq_list[site-1] = "N"
+            middle = list(seq[args.begin_length:-args.end_length])
+            seq_list = start + middle + end
+            for site in args.mask_sites:
+                seq_list[site - 1] = "N"
             record.seq = Seq("".join(seq_list))
             Bio.SeqIO.write(record, outfile, 'fasta')
