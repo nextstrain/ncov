65ceff148bc909daa46dfeb938ade5bb63a732d2
terrycojones <terry@jon.es>
2020-03-10 00:29:02 +0100

    Minor changes to narrative-pdf-screens.sh

--- scripts/narrative-pdf-screens.sh
+++ scripts/narrative-pdf-screens.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
-set -e
+
+set -Eeuo pipefail
+
 ADDRESS=$1
 PARTIAL_FNAME=$2
 
-echo "Make sure that you have auspice / nextstrain running at so that ${ADDRESS} is valid!"
+echo "Make sure that you have auspice / nextstrain running at so that $ADDRESS is valid!"
 echo "(e.g. from the 'ncov' directory run 'auspice view --datasetDir auspice --narrativeDir narratives'"
-echo "This script will save PDFs starting with the prefix ${PARTIAL_FNAME}"
-echo ""
+echo "This script will save PDFs starting with the prefix $PARTIAL_FNAME"
+echo
 
 # https://gs.statcounter.com/screen-resolution-stats/desktop/worldwide
-RESOLUTIONS=(3200x1350 1920x1080 1600x900 366x768 )
+RESOLUTIONS="3200x1350 1920x1080 1600x900 366x768"
 #             james'     iphone+            iphone
 
-for RES in ${RESOLUTIONS[@]}; do
-  F="${PARTIAL_FNAME}.${RES}.pdf"
-  echo ""
+for RES in $RESOLUTIONS; do
+  F="$PARTIAL_FNAME.$RES.pdf"
+  echo
   echo "-------------------------------------"
-  echo "Making ${F}"
-  echo ""
-  decktape generic --load-pause 3000 --key ArrowDown --size ${RES} ${ADDRESS} ${F}
+  echo "Making $F"
+  echo
+  decktape generic --load-pause 3000 --key ArrowDown --size $RES "$ADDRESS" "$F"
 done
-
-exit 0
