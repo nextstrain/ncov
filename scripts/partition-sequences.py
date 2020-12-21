ae439a85089b00949130f402039c20d530cbf3e0
John Huddleston <huddlej@gmail.com>
2020-12-21 11:06:05 -0800

    Reduce memory used by sequence partition script
    Although we don't use this sequence partitioning script anymore, we
    might in the future. Its original implementation was a proof-of-concept
    that tried to read all sequences into memory to calculate how many
    partitions were needed. This commit replaces that wasteful approach with
    a simpler streaming approach that only reads a single record into memory
    at a time and uses a counter to track when a new partition is needed.

--- scripts/partition-sequences.py
+++ scripts/partition-sequences.py
@@ -8,34 +8,34 @@ from pathlib import Path
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--sequences", required=True, nargs="+", help="FASTA file of sequences to partition into smaller chunks")
+    parser.add_argument("--sequences", required=True, help="FASTA file of sequences to partition into smaller chunks")
     parser.add_argument("--sequences-per-group", required=True, type=int, help="number of sequences to include in each group")
     parser.add_argument("--output-dir", required=True, help="directory to write out partitioned sequences")
 
     args = parser.parse_args()
 
     # Read sequences with augur to benefit from additional checks for duplicates.
-    sequences = read_sequences(*args.sequences)
-
-    # Convert dictionary of sequences by id to a list, for compatibility with
-    # augur versions <9.0.0.
-    if isinstance(sequences, dict):
-        sequences = list(sequences.values())
+    sequences = SeqIO.parse(args.sequences, "fasta")
 
     # Create the requested output directory.
     output_dir = Path(args.output_dir)
     output_dir.mkdir(exist_ok=True)
 
-    # Determine partition indices.
-    indices = list(range(0, len(sequences), args.sequences_per_group))
+    i = 0
+    group_number = 0
+    for sequence in sequences:
+        if i == args.sequences_per_group:
+            print(f"Write out {i} sequences for partition {group_number}")
+            out_handle.close()
+            i = 0
+            group_number += 1
 
-    # Add a final index to represent the end of the last partition.
-    if indices[-1] != len(sequences):
-        indices.append(len(sequences))
+        if i == 0:
+            output_path = str(output_dir / Path(f"{group_number}.fasta"))
+            out_handle = open(output_path, "w")
 
-    # Partition sequences into groups of no more than the requested number.
-    for i in range(len(indices) - 1):
-        # Save partitioned sequences to a new FASTA file named after the partition number.
-        print("Write out %i sequences for partition %i" % (indices[i + 1] - indices[i], i))
-        output_path = output_dir / Path("%i.fasta" % i)
-        SeqIO.write(sequences[indices[i]:indices[i + 1]], str(output_path), 'fasta')
+        SeqIO.write(sequence, out_handle, "fasta")
+        i += 1
+    else:
+        print(f"Write out {i} sequences for partition {group_number}")
+        out_handle.close()
