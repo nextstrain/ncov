c086b9c0efaf32986c4d7a1738fb696600780192
Richard Neher <richard.neher@unibas.ch>
2021-02-01 21:21:45 +0100

    fix explicit_translation script

--- scripts/explicit_translation.py
+++ scripts/explicit_translation.py
@@ -36,8 +36,6 @@ if __name__ == '__main__':
         for n in tt.tree.find_clades():
             if n.name not in node_data:
                 node_data[n.name] = {"aa_muts":{}}
-
-        for n in tt.tree.find_clades():
             node_data[n.name]["aa_muts"][gene] = [f"{a}{p+1}{d}" for a,p,d in n.mutations]
 
     with open(args.output, 'w') as fh:
