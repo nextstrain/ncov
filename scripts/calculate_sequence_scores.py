c3c4717375832097b024e3a2e812c7fbb7e4836b
Richard Neher <richard.neher@unibas.ch>
2021-04-03 12:29:05 +0200

    add dms script

--- scripts/calculate_sequence_scores.py
+++ scripts/calculate_sequence_scores.py
@@ -3,7 +3,7 @@ from Bio import Phylo, SeqIO
 import pandas as pd
 import numpy as np
 from collections import defaultdict
-
+from augur.distance import read_distance_map
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -14,6 +14,7 @@ if __name__ == '__main__':
     parser.add_argument('--mutation-summary', type=str)
     parser.add_argument('--map', nargs='+')
     parser.add_argument('--attribute-name', nargs='+')
+    parser.add_argument('--simple-map', action='store_true')
     args = parser.parse_args()
 
     if type(args.map)==str:
@@ -24,8 +25,10 @@ if __name__ == '__main__':
 
     mutations = pd.read_csv(args.mutation_summary, sep='\t', index_col=0)['S'].fillna('')
     positions = {}
+    positions_with_muts = {}
     for s,v in mutations.items():
-        positions[s] = [int(x[1:-1])-1 for x in v.split(',')] if v else []
+        positions[s] = [int(x[1:-1]) for x in v.split(',')] if v else []
+        positions_with_muts[s] = [(int(x[1:-1]), x[0], x[-1]) for x in v.split(',')] if v else []
         if len(positions[s])>30:
             print('excluding',s)
             positions[s]=[]
@@ -33,15 +36,25 @@ if __name__ == '__main__':
     L=1400
     scores = defaultdict(dict)
 
-    for map_file, attribute in zip(args.map, args.attribute_name):
-        with open(map_file) as fh:
-            map = json.load(fh)
-        weights = map['default']*np.ones(L)
-        for k,v in map["map"]['S'].items():
-            weights[int(k)-1] = v
+    if args.simple_map:
+        for map_file, attribute in zip(args.map, args.attribute_name):
+            with open(map_file) as fh:
+                map = json.load(fh)
+            weights = map['default']*np.ones(L)
+            for k,v in map["map"]['S'].items():
+                weights[int(k)-1] = v
 
-        for s,pos in positions.items():
-            scores[s][attribute] = np.sum(weights[pos])
+            for s,pos in positions.items():
+                scores[s][attribute] = np.sum(weights[pos])
+    else:
+        for map_file, attribute in zip(args.map, args.attribute_name):
+            dmap = read_distance_map(map_file)
+            for s, muts in positions_with_muts.items():
+                val = 0
+                for pos, wt, m in muts:
+                    if pos in dmap["map"]:
+                        val += dmap["map"][pos].get((wt,m),0)
+                scores[s][attribute] = val
 
     d = pd.DataFrame(scores).T
 
