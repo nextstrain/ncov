1f380825ea7785d05a11e3762f004554eafb6994
sidneymbell <sidneymb@uw.edu>
2021-04-12 12:56:54 -0700

    make attr name a param

--- scripts/make_pangolin_node_data.py
+++ scripts/make_pangolin_node_data.py
@@ -18,13 +18,14 @@ if __name__ == '__main__':
     )
     parser.add_argument("--pangolineages", type=str, required=True, help="pangolineages.csv")
     parser.add_argument("--node_data_outfile", type=str, help="pangolineages.json")
+    parser.add_argument("--attribute_name", default="pango_lineage_local", help="attribute name for pangolin lineage annotations in the output JSON")
     args = parser.parse_args()
 
     pangolineages = pd.read_csv(args.pangolineages)
 
     node_data = {
     "nodes": {
-    row['taxon']: {'PANGO lineage - local': row['lineage']} for idx, row in pangolineages.iterrows()
+    row['taxon']: {args.attribute_name: row['lineage']} for idx, row in pangolineages.iterrows()
         }
     }
 
