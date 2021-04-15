697a0316f48e8a945d5efadce10ad94162eb8daa
Amandeep Bhangal <adbhangal@gmail.com>
2021-04-15 07:07:43 -0700

    Adding New Singleton Design Pattern to construct recency

--- scripts/construct-recency-from-submission-date.py
+++ scripts/construct-recency-from-submission-date.py
@@ -3,23 +3,39 @@ from datetime import datetime
 from augur.utils import read_metadata
 import json
 
-def get_recency(date_str, ref_date):
-    date_submitted = datetime.strptime(date_str, '%Y-%m-%d').toordinal()
-    ref_day = ref_date.toordinal()
 
-    delta_days = ref_day - date_submitted
-    if delta_days<=0:
-        return 'New'
-    elif delta_days<3:
-        return '1-2 days ago'
-    elif delta_days<8:
-        return '3-7 days ago'
-    elif delta_days<15:
-        return 'One week ago'
-    elif delta_days<31:
-        return 'One month ago'
-    elif delta_days>=31:
-        return 'Older'
+#class applying SINGLETON Design Pattern
+class Recency:
+    __instance_recency = None   #instance Variable
+
+    #create instace of class only if previously dont exist else create new
+    def __init__(self):
+        if Recency.__instance_recency is None:
+            Recency.__instance_recency = self
+         
+        
+    @staticmethod
+    def get_recency(date_str, ref_date):
+        get_recency.date_submitted = datetime.strptime(date_str, '%Y-%m-%d').toordinal()
+        get_recency.ref_day = ref_date.toordinal()
+
+        get_recency.delta_days = ref_day - date_submitted
+        if delta_days<=0:
+        get_recency.__instance_recency = 'New'
+        elif delta_days<3:
+            get_recency.__instance_recency= '1-2 days ago'
+        elif delta_days<8:
+        get_recency.__instance_recency= '3-7 days ago'
+        elif delta_days<15:
+            get_recency.__instance_recency= 'One week ago'
+        elif delta_days<31:
+        get_recency.__instance_recency= 'One month ago'
+        elif delta_days>=31:
+            get_recency.__instance_recency= 'Older'
+
+        if not Recency.__instance_recency:
+            Recency()         
+        return Recency.__instance_recency        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -35,10 +51,12 @@ if __name__ == '__main__':
 
     node_data = {'nodes':{}}
     ref_date = datetime.now()
+    
 
     for strain, d in meta.items():
-        if 'date_submitted' in d and d['date_submitted']:
-            node_data['nodes'][strain] = {'recency': get_recency(d['date_submitted'], ref_date)}
-
+        if 'date_submitted' in d and d['date_submitted']:       
+            node_data['nodes'][strain] = {'recency': Recency.get_recency(d['date_submitted'], ref_date)}
+    #calling get_recency method 
+    
     with open(args.output, 'wt') as fh:
         json.dump(node_data, fh)
