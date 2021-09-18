140a19a3dfc7dc88d549b96b19db596522e20cb1
Trevor Bedford <trevor@bedford.io>
2021-09-18 13:57:18 -0700

    Include "mutational fitness" coloring
    This commit uses recent Obermeyer et al model to assign fitness across nodes in the phylogeny based on constituent mutations. This is implemented by:
    1. Running the supplied parse_mutational_fitness_tsv_into_distance_map.py on Supplementary Data S2 from Obermeyer et al (https://www.medrxiv.org/content/10.1101/2021.09.07.21263228v1)
    2. Including a new "mutational_fitness" rule that runs augur distance on node sequences using this map as input
    3. Updates Auspice config with "mutational_fitness" coloring
    4. Update default "genes" to include all genes from GenBank reference to correspond with mutations required for Obermeyer et al model

---
+++ scripts/developer_scripts/parse_mutational_fitness_tsv_into_distance_map.py
@@ -0,0 +1,55 @@
+"""
+Supplementary data S2 from Obermeyer et al (https://www.medrxiv.org/content/10.1101/2021.09.07.21263228v1)
+is a TSV table that maps mutations such as "S:D614G" to an estimate of "Δ log R".
+Here, we convert this TSV table into a JSON compatable with the augur distance command.
+"""
+import argparse
+import pandas as pd
+import json
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Mask initial bases from alignment FASTA",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("--input", required=True, help="TSV file of mutational effects")
+    parser.add_argument("--output", required=True, help="JSON file for augur distance")
+    args = parser.parse_args()
+
+    # collect simple string mapping from TSV, ie
+    # 'S:A522V': -0.00661378
+    # 'ORF1a:T4304I': 0.00353199
+    string_mapping = {}
+    if args.input:
+        df = pd.read_csv(args.input, delimiter='\t')
+        for index, row in df.iterrows():
+            string_mapping[row["mutation"]] = float(row["Δ log R"])
+
+    # convert simple string mapping into structured mapping required by augur distance, ie
+    # "map": {
+    #   "S": {
+    #     "522": [
+    #       {
+    #         "from": "A",
+    #         "to": "V",
+    #         "weight": -0.00661378
+    #       },
+    structured_mapping = {}
+    for mutation, delta_log_R in string_mapping.items():
+        gene, aa_change = mutation.split(":")
+        from_aa = aa_change[0]
+        to_aa = aa_change[-1]
+        pos_aa = aa_change[1:-1]
+        if gene not in structured_mapping:
+            structured_mapping[gene] = {}
+        if pos_aa not in structured_mapping[gene]:
+            structured_mapping[gene][pos_aa] = []
+        entry = { "from": from_aa, "to": to_aa, "weight": delta_log_R }
+        structured_mapping[gene][pos_aa].append(entry)
+
+    # output this mapping as an augur distance compatable JSON
+    json_output = { "default": 0.0 }
+    json_output["map"] = structured_mapping
+
+    with open(args.output, 'w') as f:
+        json.dump(json_output, f, indent=2)
