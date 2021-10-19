3213e712e6f932582475e3455ca91a3a490041c9
John Huddleston <huddlej@gmail.com>
2021-10-19 15:36:33 -0700

    Add GISAID's id field as default
    Adds GISAID's "Accession ID" field as a default database id column for
    the sanitize metadata step. This aligns the defaults in the script with
    the values in `defaults/parameters.yaml` and fixes a bug caused by a
    mismatch in this script and the data preparation guide where field
    renaming used to happen before deduplication. Now that deduplication
    occurs first, we need to support the original database id field names
    instead of assuming the renamed fields are available.
    
    Fixes #761

--- scripts/sanitize_metadata.py
+++ scripts/sanitize_metadata.py
@@ -300,7 +300,7 @@ if __name__ == '__main__':
     )
     parser.add_argument("--metadata", required=True, help="metadata to be sanitized")
     parser.add_argument("--metadata-id-columns", default=["strain", "name", "Virus name"], nargs="+", help="names of valid metadata columns containing identifier information like 'strain' or 'name'")
-    parser.add_argument("--database-id-columns", default=["gisaid_epi_isl", "genbank_accession"], nargs="+", help="names of metadata columns that store external database ids for each record (e.g., GISAID, GenBank, etc.) that can be used to deduplicate metadata records with the same strain names.")
+    parser.add_argument("--database-id-columns", default=["Accession ID", "gisaid_epi_isl", "genbank_accession"], nargs="+", help="names of metadata columns that store external database ids for each record (e.g., GISAID, GenBank, etc.) that can be used to deduplicate metadata records with the same strain names.")
     parser.add_argument("--metadata-chunk-size", type=int, default=100000, help="maximum number of metadata records to read into memory at a time. Increasing this number can speed up filtering at the cost of more memory used.")
     parser.add_argument("--error-on-duplicate-strains", action="store_true", help="exit with an error if any duplicate strains are found. By default, duplicates are resolved by preferring most recent accession id or the last record.")
     parser.add_argument("--parse-location-field", help="split the given GISAID location field on '/' and create new columns for region, country, etc. based on available data. Replaces missing geographic data with '?' values.")
