650820252030f23267dd6ba8f5d207ff3cbf466a
Victor Lin <13424970+victorlin@users.noreply.github.com>
2022-04-04 12:04:11 -0700

    Add copy button to code blocks
    Using https://pypi.org/project/sphinx-copybutton/
    
    Also https://github.com/nextstrain/docs.nextstrain.org/commit/cc2e2a85c66e36591d83b34b7195b1eff18b3ce7

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -61,6 +61,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx_markdown_tables',
     'sphinx.ext.intersphinx',
+    'sphinx_copybutton',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
