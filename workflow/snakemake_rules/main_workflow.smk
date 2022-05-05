mjfos2r/ltu-ncov

Template auspice config files for our core builds
The auspice config files were almost duplicates of each other, resulting
in 14 very similar JSONs across the open & genbank profiles. This made
it harder than it should be to change config values (e.g. see
https://github.com/nextstrain/ncov/pull/916) and also made it all-too-
easy for things to fall out-of-sync.

The adds a custom rule which is only run for nextstrain core builds.
The rule is intended to be human readable and makes it clear which
are the values which differ across open/genbank builds as well as across
the different builds within those profiles. The vast majority of the
config is unchanged.

There is one change in content made here: The orderings of {region, country,
division} colorings are now in that order for both profiles; previously
the open profile had them reversed.

workflow/snakemake_rules/main_workflow.smk
@@ -1360,13 +1360,27 @@ rule build_description:
                 > {output.description:q}
         """
 
+def get_auspice_config(w):
+    """
+    Auspice configs are chosen via this heirarchy:
+    1. A build-specific JSON
+    2. A `make_auspice_config` rule, if it exists
+    3. The file specified in the `config.files` dict
+    """
+    if "auspice_config" in config["builds"].get(w.build_name, {}):
+        return config["builds"][w.build_name]["auspice_config"]
+    if "make_auspice_config" in rules.__dict__:
+        return rules.make_auspice_config.output
+    return config["files"]["auspice_config"]
+
+
 rule export:
     message: "Exporting data files for Auspice"
     input:
         tree = rules.refine.output.tree,
         metadata="results/{build_name}/metadata_adjusted.tsv.xz",
         node_data = _get_node_data_by_wildcards,
-        auspice_config = lambda w: config["builds"][w.build_name]["auspice_config"] if "auspice_config" in config["builds"].get(w.build_name, {}) else config["files"]["auspice_config"],
+        auspice_config = get_auspice_config,
         colors = lambda w: config["builds"][w.build_name]["colors"] if "colors" in config["builds"].get(w.build_name, {}) else ( config["files"]["colors"] if "colors" in config["files"] else rules.colors.output.colors.format(**w) ),
         lat_longs = config["files"]["lat_longs"],
         description = rules.build_description.output.description

nextstrain/ncov

workflow/snakemake_rules/main_workflow.smk
@@ -1381,13 +1381,27 @@ rule build_description:
         with open(output.description, "w", encoding = "utf-8") as o:
             o.write(template.safe_substitute(context))
 
+def get_auspice_config(w):
+    """
+    Auspice configs are chosen via this heirarchy:
+    1. A build-specific JSON
+    2. An `auspice_config` rule, if it exists
+    3. The file specified in the `config.files` dict
+    """
+    if "auspice_config" in config["builds"].get(w.build_name, {}):
+        return config["builds"][w.build_name]["auspice_config"]
+    if "auspice_config" in rules.__dict__:
+        return rules.auspice_config.output
+    return config["files"]["auspice_config"]
+
+
 rule export:
     message: "Exporting data files for Auspice"
     input:
         tree = rules.refine.output.tree,
         metadata="results/{build_name}/metadata_adjusted.tsv.xz",
         node_data = _get_node_data_by_wildcards,
-        auspice_config = lambda w: config["builds"][w.build_name]["auspice_config"] if "auspice_config" in config["builds"].get(w.build_name, {}) else config["files"]["auspice_config"],
+        auspice_config = get_auspice_config,
         colors = lambda w: config["builds"][w.build_name]["colors"] if "colors" in config["builds"].get(w.build_name, {}) else ( config["files"]["colors"] if "colors" in config["files"] else rules.colors.output.colors.format(**w) ),
         lat_longs = config["files"]["lat_longs"],
         description = rules.build_description.output.description