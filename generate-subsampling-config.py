993d5ccc4359ed89cd39a3f4efa9bb04bbc9523b
Victor Lin <13424970+victorlin@users.noreply.github.com>
2024-03-29 15:11:29 -0700

    🚧 add weighted sampling between samples

--- generate-subsampling-config.py
+++ generate-subsampling-config.py
@@ -3,24 +3,21 @@ from pathlib import Path
 from typing import List, Optional
 import yaml
 
+# Avoid writing aliases for identical objects.
+# https://stackoverflow.com/a/30682604
+yaml.Dumper.ignore_aliases = lambda *args: True
+
 
 SUBSAMPLING_CONFIG_DIR = 'subsampling/'
 Path(SUBSAMPLING_CONFIG_DIR).mkdir(exist_ok=True)
 
 
-class Sample:
-    name: str
-    group_by: Optional[List[str]]
-    size: Optional[int]
+class Filters:
     min_date: Optional[str]
     max_date: Optional[str]
     excludes: Optional[List[str]]
 
-    def __init__(self, name, group_by=None, size=None,
-                 min_date=None, max_date=None, excludes=None):
-        self.name = name
-        self.group_by = group_by
-        self.size = size
+    def __init__(self, min_date=None, max_date=None, excludes=None):
         self.min_date = min_date
         self.max_date = max_date
         self.excludes = excludes
@@ -29,28 +26,61 @@ class Sample:
     def to_dict(self):
         options = dict()
 
-        if self.group_by:
-            options['group_by'] = ' '.join(field for field in self.group_by)
-
-        if self.size:
-            options['max_sequences'] = self.size
-
         if self.min_date:
-            options['min_date'] = f"--min-date {self.min_date}"
+            options['min_date'] = self.min_date
 
         if self.max_date:
-            options['max_date'] = f"--max-date {self.max_date}"
+            options['max_date'] = self.max_date
 
         if self.excludes:
-            options['exclude'] = f"--exclude-where {' '.join(repr(exclude) for exclude in self.excludes)}"
+            options['exclude'] = self.excludes
+
+        return options
+
+File = str
+
+class Sample:
+    name: str
+    filters: Optional[Filters]
+    weight: Optional[int]
+    weighted_sampling: Optional[File]
+    uniform_sampling: Optional[List[str]]
+
+    def __init__(self, name, weight=None, weighted_sampling=None, uniform_sampling=None, filters=None):
+        self.name = name
+        self.weight = weight
+        self.weighted_sampling = weighted_sampling
+        self.uniform_sampling = uniform_sampling
+        self.filters = filters
+
+    def to_dict(self):
+        options = dict()
+
+        if self.weight:
+            options['weight'] = self.weight
+
+        if self.filters:
+            options['filters'] = self.filters.to_dict()
+
+        if self.weighted_sampling:
+            options['weighted_sampling'] = self.weighted_sampling
+
+        if self.uniform_sampling:
+            options['uniform_sampling'] = self.uniform_sampling
 
         return options
 
 
 class Config:
-    samples: List[Sample]
-    def __init__(self):
-        self.samples = []
+    size: int
+    samples: Optional[List[Sample]]
+
+    def __init__(self, size, samples=None):
+        if samples == None:
+            samples = []
+
+        self.size = size
+        self.samples = samples
 
     def add(self, new_sample: Sample):
         if any(new_sample.name == sample.name for sample in self.samples):
@@ -60,13 +90,14 @@ class Config:
 
     def to_dict(self):
         return {
+            'size': self.size,
             'samples': {
                 sample.name: sample.to_dict() for sample in self.samples
             }
         }
 
     def to_file(self, path):
-        print(f'Writing {path}.  n={sum(sample.size for sample in self.samples)}')
+        print(f'Writing {path}.')
         with open(path, 'w') as f:
             yaml.dump(self.to_dict(), f, sort_keys=False)
 
@@ -115,11 +146,9 @@ def write_region_time_builds():
         build_name = f"{region.lower().replace(' ', '-')}_{time.lower()}"
         filename = Path(SUBSAMPLING_CONFIG_DIR, f"{build_name}.yaml")
 
-        config = Config()
-
         # Global gets special treatment because it is not a region.
         if region == 'Global':
-            target_size = 5150
+            config = Config(size=5150)
 
             locations = [
                 'Africa',
@@ -163,56 +192,49 @@ def write_region_time_builds():
                 for location in locations:
                     config.add(Sample(
                         name=location.lower().replace(' ', '_'),
-                        group_by=[
+                        uniform_sampling=[
                             GROUP_BY_GEOGRAPHICAL_RESOLUTION[location],
                             'year',
                             'month',
                         ],
-                        size=int(
-                            target_size
-                            * weights[location] / sum_location_weights
-                        ),
-                        excludes=excludes[location],
+                        weight=weights[location],
+                        filters=Filters(excludes=excludes[location]),
                     ))
             else:
                 # Early sequences
                 for location in locations:
                     config.add(Sample(
                         name=f"{location.lower().replace(' ', '_')}_early",
-                        group_by=[
+                        uniform_sampling=[
                             GROUP_BY_GEOGRAPHICAL_RESOLUTION[location],
                             'year',
                             'month',
                         ],
-                        size=int(
-                            target_size
-                            * WEIGHT_EARLY / (WEIGHT_EARLY + WEIGHT_RECENT)
-                            * weights[location] / sum_location_weights
+                        weight=(WEIGHT_EARLY * weights[location]),
+                        filters=Filters(
+                            excludes=excludes[location],
+                            max_date=time,
                         ),
-                        max_date=time,
-                        excludes=excludes[location],
                     ))
 
                 # Recent sequences
                 for location in locations:
                     config.add(Sample(
                         name=f"{location.lower().replace(' ', '_')}_recent",
-                        group_by=[
+                        uniform_sampling=[
                             GROUP_BY_GEOGRAPHICAL_RESOLUTION[location],
                             GROUP_BY_RECENT_TEMPORAL_RESOLUTION[time],
                         ],
-                        size=int(
-                            target_size
-                            * WEIGHT_RECENT / (WEIGHT_EARLY + WEIGHT_RECENT)
-                            * weights[location] / sum_location_weights
+                        weight=(WEIGHT_RECENT * weights[location]),
+                        filters=Filters(
+                            excludes=excludes[location],
+                            min_date=time,
                         ),
-                        min_date=time,
-                        excludes=excludes[location],
                     ))
 
         # Asia gets special treatment because two countries must be weighted differently.
         elif region == 'Asia':
-            target_size = 4375
+            config = Config(size=4375)
 
             locations = [
                 'Asia',
@@ -240,33 +262,30 @@ def write_region_time_builds():
                 for location in locations:
                     config.add(Sample(
                         name=location.lower().replace(' ', '_'),
-                        group_by=[
+                        uniform_sampling=[
                             # TODO: use GROUP_BY_GEOGRAPHICAL_RESOLUTION?
                             'division',
                             'year',
                             'month',
                         ],
-                        size=int(
-                            target_size
-                            * WEIGHT_FOCAL / (WEIGHT_FOCAL + WEIGHT_CONTEXTUAL)
-                            * weights[location] / sum_location_weights
+                        weight=(WEIGHT_FOCAL * weights[location]),
+                        filters=Filters(
+                            excludes=excludes[location],
                         ),
-                        excludes=excludes[location],
                     ))
                 
                 # Contextual sequences from the rest of the world
                 config.add(Sample(
                     name='context',
-                    group_by=[
+                    uniform_sampling=[
                         'country',
                         'year',
                         'month',
                     ],
-                    size=int(
-                        target_size
-                        * WEIGHT_CONTEXTUAL / (WEIGHT_FOCAL + WEIGHT_CONTEXTUAL)
+                    weight=WEIGHT_CONTEXTUAL,
+                    filters=Filters(
+                        excludes=['region=Asia'],
                     ),
-                    excludes=['region=Asia'],
                 ))
 
             else:
@@ -274,182 +293,158 @@ def write_region_time_builds():
                 for location in locations:
                     config.add(Sample(
                         name=f"{location.lower().replace(' ', '_')}_early",
-                        group_by=[
+                        uniform_sampling=[
                             # TODO: use GROUP_BY_GEOGRAPHICAL_RESOLUTION?
                             'division',
                             'year',
                             'month',
                         ],
-                        size=int(
-                            target_size
-                            * WEIGHT_EARLY / (WEIGHT_EARLY + WEIGHT_RECENT)
-                            * WEIGHT_FOCAL / (WEIGHT_FOCAL + WEIGHT_CONTEXTUAL)
-                            * weights[location] / sum_location_weights
+                        weight=(WEIGHT_EARLY * WEIGHT_FOCAL * weights[location]),
+                        filters=Filters(
+                            max_date=time,
+                            excludes=excludes[location],
                         ),
-                        max_date=time,
-                        excludes=excludes[location],
                     ))
 
                 # Early contextual sequences from the rest of the world
                 config.add(Sample(
                     name='context_early',
-                    group_by=[
+                    uniform_sampling=[
                         'country',
                         'year',
                         'month',
                     ],
-                    size=int(
-                        target_size
-                        * WEIGHT_EARLY / (WEIGHT_EARLY + WEIGHT_RECENT)
-                        * WEIGHT_CONTEXTUAL / (WEIGHT_FOCAL + WEIGHT_CONTEXTUAL)
+                    weight=(WEIGHT_EARLY * WEIGHT_CONTEXTUAL),
+                    filters=Filters(
+                        max_date=time,
+                        excludes=['region=Asia'],
                     ),
-                    max_date=time,
-                    excludes=['region=Asia'],
                 ))
 
                 # Recent focal sequences for region
                 for location in locations:
                     config.add(Sample(
                         name=f"{location.lower().replace(' ', '_')}_recent",
-                        group_by=[
+                        uniform_sampling=[
                             # TODO: use GROUP_BY_GEOGRAPHICAL_RESOLUTION?
                             'division',
                             # TODO: use GROUP_BY_RECENT_TEMPORAL_RESOLUTION?
                             'year',
                             'month',
                         ],
-                        size=int(
-                            target_size
-                            * WEIGHT_RECENT / (WEIGHT_EARLY + WEIGHT_RECENT)
-                            * WEIGHT_FOCAL / (WEIGHT_FOCAL + WEIGHT_CONTEXTUAL)
-                            * weights[location] / sum_location_weights
+                        weight=(WEIGHT_RECENT * WEIGHT_FOCAL * weights[location]),
+                        filters=Filters(
+                            min_date=time,
+                            excludes=excludes[location],
                         ),
-                        min_date=time,
-                        excludes=excludes[location],
                     ))
 
                 # Recent contextual sequences from the rest of the world
                 config.add(Sample(
                     name='context_recent',
-                    group_by=[
+                    uniform_sampling=[
                         'country',
                         # TODO: use GROUP_BY_RECENT_TEMPORAL_RESOLUTION?
                         'year',
                         'month',
                     ],
-                    size=int(
-                        target_size
-                        * WEIGHT_RECENT / (WEIGHT_EARLY + WEIGHT_RECENT)
-                        * WEIGHT_CONTEXTUAL / (WEIGHT_FOCAL + WEIGHT_CONTEXTUAL)
+                    weight=(WEIGHT_RECENT * WEIGHT_CONTEXTUAL),
+                    filters=Filters(
+                        min_date=time,
+                        excludes=['region=Asia'],
                     ),
-                    min_date=time,
-                    excludes=['region=Asia'],
                 ))
 
         # Everything else is a "standard" region with dynamic geographical/temporal grouping.
         else:
-            target_size = 4000
+            config = Config(size=4000)
 
             if time == 'all-time':
                 # Focal sequences for region
                 config.add(Sample(
                     name='focal',
-                    group_by=[
+                    uniform_sampling=[
                         GROUP_BY_GEOGRAPHICAL_RESOLUTION[region],
                         'year',
                         'month',
                     ],
-                    size=int(
-                        target_size
-                        * WEIGHT_FOCAL / (WEIGHT_FOCAL + WEIGHT_CONTEXTUAL)
+                    weight=WEIGHT_FOCAL,
+                    filters=Filters(
+                        excludes=[f'region!={region}'],
                     ),
-                    excludes=[f'region!={region}']
                 ))
 
                 # Contextual sequences from the rest of the world
                 config.add(Sample(
                     name='context',
-                    group_by=[
+                    uniform_sampling=[
                         'country',
                         'year',
                         'month',
                     ],
-                    size=int(
-                        target_size
-                        * WEIGHT_CONTEXTUAL / (WEIGHT_FOCAL + WEIGHT_CONTEXTUAL)
+                    weight=WEIGHT_CONTEXTUAL,
+                    filters=Filters(
+                        excludes=[f'region={region}'],
                     ),
-                    excludes=[f'region={region}']
                 ))
             else:
                 # Early focal sequences for region
                 config.add(Sample(
                     name='focal_early',
-                    group_by=[
+                    uniform_sampling=[
                         GROUP_BY_GEOGRAPHICAL_RESOLUTION[region],
                         'year',
                         'month',
                     ],
-                    size=int(
-                        target_size
-                        * WEIGHT_EARLY / (WEIGHT_EARLY + WEIGHT_RECENT)
-                        * WEIGHT_FOCAL / (WEIGHT_FOCAL + WEIGHT_CONTEXTUAL)
+                    weight=(WEIGHT_EARLY * WEIGHT_FOCAL),
+                    filters=Filters(
+                        max_date=time,
+                        excludes=[f'region!={region}'],
                     ),
-                    max_date=time,
-                    excludes=[f'region!={region}'],
                 ))
 
                 # Early contextual sequences from the rest of the world
                 config.add(Sample(
                     name='context_early',
-                    group_by=[
+                    uniform_sampling=[
                         'country',
                         'year',
                         'month',
                     ],
-                    size=int(
-                        target_size
-                        * WEIGHT_EARLY / (WEIGHT_EARLY + WEIGHT_RECENT)
-                        * WEIGHT_CONTEXTUAL / (WEIGHT_FOCAL + WEIGHT_CONTEXTUAL)
+                    weight=(WEIGHT_EARLY * WEIGHT_CONTEXTUAL),
+                    filters=Filters(
+                        max_date=time,
+                        excludes=[f'region={region}'],
                     ),
-                    max_date=time,
-                    excludes=[f'region={region}'],
                 ))
 
                 # Recent focal sequences for region
                 config.add(Sample(
                     name='focal_recent',
-                    group_by=[
+                    uniform_sampling=[
                         GROUP_BY_GEOGRAPHICAL_RESOLUTION[region],
                         GROUP_BY_RECENT_TEMPORAL_RESOLUTION[time],
                     ],
-                    size=int(
-                        target_size
-                        * WEIGHT_RECENT / (WEIGHT_EARLY + WEIGHT_RECENT)
-                        * WEIGHT_FOCAL / (WEIGHT_FOCAL + WEIGHT_CONTEXTUAL)
+                    weight=(WEIGHT_RECENT * WEIGHT_FOCAL),
+                    filters=Filters(
+                        min_date=time,
+                        excludes=[f'region!={region}'],
                     ),
-                    min_date=time,
-                    excludes=[f'region!={region}'],
                 ))
 
                 # Recent contextual sequences from the rest of the world
                 config.add(Sample(
                     name='context_recent',
-                    group_by=[
+                    uniform_sampling=[
                         'country',
                         GROUP_BY_RECENT_TEMPORAL_RESOLUTION[time],
                     ],
-                    size=int(
-                        target_size
-                        * WEIGHT_RECENT / (WEIGHT_EARLY + WEIGHT_RECENT)
-                        * WEIGHT_CONTEXTUAL / (WEIGHT_FOCAL + WEIGHT_CONTEXTUAL)
+                    weight=(WEIGHT_RECENT * WEIGHT_CONTEXTUAL),
+                    filters=Filters(
+                        min_date=time,
+                        excludes=[f'region={region}'],
                     ),
-                    min_date=time,
-                    excludes=[f'region={region}'],
                 ))
-
-        # Double check the total sample size.
-        total_size = sum(sample.size for sample in config.samples)
-        assert target_size == total_size
 
         config.to_file(filename)
 
@@ -458,7 +453,7 @@ def write_reference_build():
     config = Config()
     config.add(Sample(
         name='clades',
-        group_by=['Nextstrain_clade'],
+        uniform_sampling=['Nextstrain_clade'],
         size=300,
     ))
     filename = Path(SUBSAMPLING_CONFIG_DIR, f"reference.yaml")
@@ -467,7 +462,7 @@ def write_reference_build():
 
 def main():
     write_region_time_builds()
-    write_reference_build()
+    # write_reference_build()
     # TODO: 100k builds
     # TODO: CI builds
     # TODO: country builds
