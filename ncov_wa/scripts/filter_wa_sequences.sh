af25fe35089f2efc503aa7d4e25397f3e99911e1
Lauren Frisbie <lauren.frisbie@doh.wa.gov>
2025-07-02 11:24:15 -0700

    Update wa files
    Changes in config file, scripts and workflow smk files to include 1 year of data

--- ncov_wa/scripts/filter_wa_sequences.sh
+++ ncov_wa/scripts/filter_wa_sequences.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
+input_fasta_file="$1" #Nextstrain remote dataset file
+filtered_metadata_file="$2" #WA metadata file
+tmp_file="$3"
+output_fasta_file="$4" #Filtered WA fasta file
 
-input_file="$1"
-output_file="$2"
-pattern="USA/WA"
+# Decompress fasta files into temp
+xzcat "$input_fasta_file" > temp_decompressed.fasta
 
-xzcat "$input_file" | awk -v pattern="$pattern" '
-BEGIN { RS=">"; ORS="" }
-NR > 1 && $0 ~ pattern {
-  split($0, header_seq, "\n");
-  header = header_seq[1];
-  seq = "";
-  for (i = 2; i <= length(header_seq); i++) {
-    seq = seq header_seq[i];
-  }
-  if (length(seq) >= 27000) {
-    print ">" header "\n" seq "\n";
-  }
-}' > "$output_file"
+# 1. Extract strain names (assumes 'strain' is the first column)
+cut -f1 "$filtered_metadata_file" | tail -n +2 | tr -d '\r' > "$tmp_file" #ensures temp files does not contain and \r characters
+tmp_file=$(echo "$tmp_file" | tr -d '\r')
 
-
-echo "WA seqs have been filtered and written to $output_file."
+#matches strain name to fasta header
+seqtk subseq temp_decompressed.fasta "$tmp_file" > "$output_fasta_file"
+echo "WA seqs have been filtered and written to $output_fasta_file."
